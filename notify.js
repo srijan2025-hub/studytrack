51ddaebca79ddccec414e41c39580eb8c6899942
srijan2025-hub <srijangithub@gmail.com>
2025-12-26 17:33:03 +0530

    Add notification scheduling functionality

---
+++ notify.js
@@ -0,0 +1,19 @@
+if ("Notification" in window) {
+  Notification.requestPermission();
+}
+
+function scheduleNotification(time, text) {
+  if (Notification.permission !== "granted") return;
+
+  const now = new Date();
+  const [h, m] = time.split(":");
+  const target = new Date();
+  target.setHours(h, m, 0, 0);
+
+  const delay = target - now;
+  if (delay > 0) {
+    setTimeout(() => {
+      new Notification("Routine Reminder", { body: text });
+    }, delay);
+  }
+}
