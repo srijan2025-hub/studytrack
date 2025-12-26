2ed8725ba917bf2513695e47247354beda2a63bc
srijan2025-hub <srijangithub@gmail.com>
2025-12-26 17:35:38 +0530

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "routine-cache-v1";
+const ASSETS = [
+  "./",
+  "index.html",
+  "style.css",
+  "routine.js",
+  "notify.js",
+  "graph.js",
+  "app.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
